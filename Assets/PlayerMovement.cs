14438dc2d4b784e0d9b9890053ed26edde796316
ASandhuZero <as7687a@american.edu>
2018-02-08 13:36:40 -0500

    Test4

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float posX = transform.position.z + 1;
+		transform.position = new Vector3(0, 0, posX);
+		print(transform.position.x);
+	}
+}
